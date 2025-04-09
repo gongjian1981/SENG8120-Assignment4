99c6d50305abea978b9758d48ed4ada4af4e6762
Jian Gong <gongjian1981@gmail.com>
2025-04-09 15:17:16 -0400

    fix: adjust position of length with width

--- src/calculateModernisticCanvasSize/calculateModernisticCanvasSize.test.ts
+++ src/calculateModernisticCanvasSize/calculateModernisticCanvasSize.test.ts
@@ -2,7 +2,7 @@ import calculateModernisticCanvasSize from "./calculateModernisticCanvasSize";
 
 describe("calculateModernisticCanvasSize", () => {
   test("returns correct result", () => {
-    const result = calculateModernisticCanvasSize("2", "3");
+    const result = calculateModernisticCanvasSize("3", "2");
 
     expect(result).toEqual([2, 4]);
   })
