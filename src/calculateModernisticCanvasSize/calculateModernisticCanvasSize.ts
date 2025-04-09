1d754617865674f2fabb4670f502b9589f08d868
Jian Gong <gongjian1981@gmail.com>
2025-04-09 15:19:30 -0400

    fix: adjust position of length with width

--- src/calculateModernisticCanvasSize/calculateModernisticCanvasSize.ts
+++ src/calculateModernisticCanvasSize/calculateModernisticCanvasSize.ts
@@ -4,7 +4,7 @@ export default function calculateModernisticCanvasSize(
 ) {
   var lengthInt = parseInt(length);
   var widthInt = parseInt(width);
-  var area1 = widthInt / 3 * lengthInt;
-  var area2 = widthInt / 3 * lengthInt * 2;
+  var area1 = lengthInt / 3 * widthInt;
+  var area2 = lengthInt / 3 * widthInt * 2;
   return [area1, area2];
 }
